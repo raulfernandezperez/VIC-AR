d5f81cff0d68bb61dfdf6f750ce4ad867d9f7ad4
Raúl Fernández <raul.fernandezperez@outlook.com>
2020-08-30 23:49:30 +0200

    Sistema para seleccionar movimientos AR

---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+
+public class MovementManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [TitleGroup("Animator")]
+    [SerializeField] private Animator animatorController;
+    [SerializeField] [ReadOnly] private bool changedMovement;
+
+    [TitleGroup("Settings")]
+    [SerializeField] private Animator currentMovement;
+    [SerializeField] private Animator startMovement;
+    [TitleGroup("Movemements")]
+    [SerializeField] [ReadOnly] private List<Animator> movements;
+
+    private void Start()
+    {
+
+        if (animatorController == null)
+        {
+            animatorController = GetComponent<Animator>();
+        }
+        changedMovement = false;
+
+        movements.Add(currentMovement);
+        movements.Add(startMovement);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            ChangeMovement();
+        }
+    }
+
+
+    public void ChangeMovement()
+    {
+        if (!changedMovement)
+        {
+            animatorController = currentMovement;
+            changedMovement = true;
+            animatorController.SetTrigger("ChangedMovement");
+            UIController.Instance.MovementText.text = animatorController.name;
+        }
+        else
+        {
+            animatorController = startMovement;
+            changedMovement = false;
+            animatorController.SetTrigger("ChangedMovement");
+            UIController.Instance.MovementText.text = animatorController.name;
+        }
+    }
+}
---
+++ Assets/UI/UIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.UI;
+using JetBrains.Annotations;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController Instance;
+
+    [TitleGroup("Buttons")]
+    [SerializeField] private Button movementButton;
+    public Button MovementButton { get => movementButton; set => movementButton = value; }
+
+    [SerializeField] private Text movementText;
+    public Text MovementText { get => movementText; set => movementText = value; }
+
+}
